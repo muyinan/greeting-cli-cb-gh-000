d3377c97c66ea7b90cd7bbd27bacc4e2642d2d75
Dorna Mu <dorna.mu@gmail.com>
2018-07-12 22:06:22 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,6 @@
 # code tdefhe #greeting method here!
-def greeting(name)
   puts "Hi! I'm HAL, what's your name?' exactly."
   name = gets.strip
+def greeting(name)
   puts "Hello, #{name}. It is nice to meet you"
 end
